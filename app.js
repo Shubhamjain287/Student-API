c638b956bb852ce02244877525c6a947d700d1ea
Shubhamjain287 <Shubhamjainpvt28@gmail.com>
2023-01-31 20:00:59 +0530

    Student API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const router = require('./src/Routers/studentRoutes');
+const cors = require('cors');
 
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
@@ -10,8 +11,13 @@ const ConnectDB = require('./src/Database/database');
 const PORT = process.env.PORT;
 ConnectDB();
 
+const corsOptions =  {
+    origin:'*'
+}
+
 app.use(express.json());
 app.use(router);
+app.use(cors(corsOptions));
 
 app.listen(PORT,()=>{
     console.log(`Connection Live on ther Server Port ${PORT}`);
